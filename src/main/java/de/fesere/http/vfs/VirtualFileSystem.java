8393a1d3c4934412e9cdbd4d4b29ca1fad19db9d
Felipe Sere <felipesere@gmail.com>
2017-03-02 22:51:46 +0000

    Use modern version of IOutils!

--- src/main/java/de/fesere/http/vfs/VirtualFileSystem.java
+++ src/main/java/de/fesere/http/vfs/VirtualFileSystem.java
@@ -5,6 +5,7 @@ import org.apache.commons.io.IOUtils;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -87,7 +88,7 @@ public class VirtualFileSystem {
   }
 
   private List<String> read(File file) throws IOException {
-    return IOUtils.readLines(new FileInputStream(file));
+    return IOUtils.readLines(new FileInputStream(file), Charset.defaultCharset());
   }
 
   public byte[] getRawBytes(String path) {
